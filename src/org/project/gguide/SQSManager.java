d5058bfa327aeb8a0d600c45b79717092d4ded49
Jackie Jin <liveaeon@gmail.com>
2013-06-02 06:32:24 -0700

    real time messaging on parse works

--- src/org/project/gguide/SQSManager.java
+++ src/org/project/gguide/SQSManager.java
@@ -1,57 +1,70 @@
 package org.project.gguide;
 
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.ExecutorService;
+import org.json.JSONObject;
 
-import android.os.AsyncTask;
+import android.content.Context;
+import android.widget.Toast;
 
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.sns.AmazonSNSClient;
-import com.amazonaws.services.sns.model.CreateTopicRequest;
-import com.amazonaws.services.sns.model.SubscribeRequest;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sqs.AmazonSQSClient;
 import com.amazonaws.services.sqs.model.CreateQueueRequest;
 import com.amazonaws.services.sqs.model.CreateQueueResult;
-import com.amazonaws.services.sqs.model.GetQueueAttributesRequest;
-import com.amazonaws.services.sqs.model.ListQueuesRequest;
-import com.amazonaws.services.sqs.model.ListQueuesResult;
 import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import com.amazonaws.services.sqs.model.ReceiveMessageResult;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
+import com.amazonaws.services.sqs.model.SendMessageResult;
 
 public class SQSManager {
 
 	private AmazonSQSClient sqsClient;
-	private final String[]  users = new String[] {"JackieJ", "RyanC"};
-	private String queueURL;
+	public static final String me = "Jacky";
+	public static final String other = "Ryan";
+	private String myQueue = new String();
+	private String otherQueue;
+	private JSONObject sMsg;
+	private Context mContext;
 	
-	public SQSManager () {
-		BasicAWSCredentials creds = new BasicAWSCredentials("AKIAI5JU3AHJTVGQODJQ", "Y7eKeWnxUlyjMhJGQRaJthEw2WNULu9W/xg+Vh0o");
+	public SQSManager (Context context) {
+		BasicAWSCredentials creds = new BasicAWSCredentials("AKIAIRQFMA3ASX7ORJMA", "9M0ZkeuxisNg/LQ2KbT50jP5PbaLy9ONmAFZBXoe");
 		sqsClient = new AmazonSQSClient(creds);
-		String myQueue = initializeQueue(sqsClient, users[0]);
-		String otherQueue = initializeQueue(sqsClient, users[1]);
+		Region region = Region.getRegion(Regions.US_WEST_2); 
+		sqsClient.setRegion(region);
 		
-		//sending and receiving messages
-		runAsync();
+		try {
+		//registration queues
+			CreateQueueRequest cqrMe = new CreateQueueRequest(me);
+			CreateQueueResult result = sqsClient.createQueue(cqrMe);
+			myQueue = result.getQueueUrl();
 		
-		//do in an infinite loop in another thread
-		sqsClient.receiveMessage(new ReceiveMessageRequest(myQueue));
+			CreateQueueRequest cqrOther = new CreateQueueRequest(other);
+			CreateQueueResult result2 = sqsClient.createQueue(cqrOther);
+			otherQueue = result2.getQueueUrl();
+		} catch (Exception exception) {
+			System.out.println("Exception  = " + exception);
+		}
 		
-		sqsClient.sendMessage(new SendMessageRequest(otherQueue, "helloWorld"));
-		
+		//Toast.makeText(mContext, "SQS "+myQueue, Toast.LENGTH_SHORT).show();
+		mContext = context;
+		try {
+			sqsClient.sendMessage(new SendMessageRequest(otherQueue, "Hell"));
+		} catch (Exception exception) {
+			System.out.println("Exception  = " + exception);
+		}
+		Toast.makeText(mContext, "SQS "+myQueue, Toast.LENGTH_SHORT).show();
 	}
-
-	private void runAsync() {
-		new AsyncTask <Void, Integer ,String>() {
-
-			@Override
-			protected String doInBackground(Void... params) {
-				return queueURL;
-				// TODO Auto-generated method stub
-				
-			}
-			
-		}.execute(null,null,null);
+	
+	public ReceiveMessageResult receiveMsg () {
+		return sqsClient.receiveMessage(new ReceiveMessageRequest(myQueue));
+	}
+	
+	public void setMsg (JSONObject msg) {
+		sMsg = msg;
+	}
+	
+	public SendMessageResult sendMsg() {
+		return sqsClient.sendMessage(new SendMessageRequest(otherQueue, sMsg.toString()));
 	}
 	
 	/**
@@ -61,18 +74,17 @@ public class SQSManager {
 	 * @param userName 
 	 * @return queue url
 	 */
-	private String initializeQueue(AmazonSQSClient sqsClient, String userName) {
-		ListQueuesResult queues = sqsClient.listQueues(new ListQueuesRequest(userName));
-		List<String> queueURLs = queues.getQueueUrls();
-		for (String queueUrl : queueURLs) {
-			System.err.println(queueUrl);
-			if (queueUrl.endsWith(userName)) {
-				return queueUrl;
-			}
+	private String initializeQueue(AmazonSQSClient sqsClient, final String userName) {
+		try {
+			CreateQueueRequest cqr = new CreateQueueRequest(userName);
+			CreateQueueResult result = this.sqsClient.createQueue(cqr);
+			Toast.makeText(mContext, "SQS created!", Toast.LENGTH_SHORT).show();
+			return result.getQueueUrl();
+		} catch (Exception exception) {
+			//Toast.makeText(mContext, "error", Toast.LENGTH_SHORT).show();
+			System.out.println("Exception = " + exception);
+			return "";
 		}
-		return sqsClient.createQueue(new CreateQueueRequest(userName)).getQueueUrl();
-
 	}
-
 
 }
